5ca08725dbf7857e0a6764f0a519d993e524abc8
Blade Boles <bladerboles@gmail.com>
2020-06-11 15:05:13 -0600

    Broke conditional rendering.  Within the next five years I will fix React's stupidity when dealing with undefined values if someone else doesn't do it before me.  Absolutely ridiculous that a framework like this doesn't have a simple built in default render for such a common issue.

--- src/Status.js
+++ src/Status.js
@@ -1,11 +1,28 @@
 import React from 'react';
 
 export default function Dashboard(props) {
-  const { profiles, characters, stats, skills } = props.values;
-  const userSignedIn = profiles[0].account_id ? profiles[0].account_id : 0;
+  const { profiles, characters, stats, skills, portfolios } = props.values;
+  console.log(profiles)
+  let userSignedIn; 
+  try {
+    userSignedIn = profiles[1].account_id 
+  }
+  catch (err) {
+    userSignedIn = 1
+  }
+
   const userCharacter = profiles.length > 1 ? characters[userSignedIn].character_name : 'Default Character';
-  const characterStats = stats[userSignedIn]
-  const characterSkills = skills[userSignedIn]
+
+  let characterCareer;
+  try {
+    characterCareer = portfolios[userSignedIn].career_path;
+  } 
+  catch (err) {
+    characterCareer = "Default Career"
+  }
+  const characterStats = stats[userSignedIn] || '';
+  const characterSkills = skills[userSignedIn] || '';
+
   // const skillsList = profiles.length > 1 && Object.keys(characterSkills)
   // const skillsMap = skillsList.length > 1 && skillsList.map(skill => <li>{skill}: {characterSkills[skill]} / 10</li>)
   // const skillsDisplay = skillsMap.length > 1 && skillsMap.slice(2)
@@ -14,7 +31,7 @@ export default function Dashboard(props) {
     <div>
       <section>
         {profiles.length > 1 && 
-          <h1>{userCharacter}, "Full Stack Developer"</h1>
+          <h1>{userCharacter}, "{characterCareer} Developer"</h1>
         }
 
         <div className="image-container">
@@ -27,39 +44,39 @@ export default function Dashboard(props) {
         <div id="resources-status">
           <h3>Resources</h3>
           <ul className="stats-list">
-            {stats.length > 1 && 
+            {characterStats && 
               <li>Funds: ${characterStats.funds}</li>
             }
-            {stats.length > 1 && 
+            {characterStats && 
               <li>Happiness: {characterStats.happiness}/100</li>
             }
-            {stats.length > 1 && 
-              <li>Connections: {characterStats.connections}</li>
+            {characterStats && 
+              <li>Connections: {characterStats.connections}/20</li>
             }
-            {stats.length > 1 && 
-              <li>Caffeine: {characterStats.energy}</li>
+            {characterStats && 
+              <li>Caffeine: {characterStats.energy}/100</li>
             }
-            {stats.length > 1 && 
-              <li>Health: {characterStats.health}</li>
+            {characterStats && 
+              <li>Health: {characterStats.health}/100</li>
             }
           </ul>
         </div>
         <div id="skills-status">
           <h3>Stats</h3>
           <ul className="skills-list">
-            {skills.length > 1 &&
+            {characterSkills &&
               <li>Front End: {characterSkills.front_end}/10</li>
             }
-            {skills.length > 1 &&
+            {characterSkills &&
               <li>Back End: {characterSkills.back_end}/10</li>
             }
-            {skills.length > 1 &&
+            {characterSkills &&
               <li>Computer Science: {characterSkills.computer_science}/10</li>
             }
-            {skills.length > 1 &&
+            {characterSkills &&
               <li>Interpersonal: {characterSkills.soft_skills}/10</li>
             }
-            {skills.length > 1 &&
+            {characterSkills &&
               <li>Luck: {characterSkills.luck}/10</li>
             }
           </ul>
