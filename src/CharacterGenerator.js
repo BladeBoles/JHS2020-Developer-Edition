d405a3fcedd4f0de057a389fd6ee04db8a3b5084
Blade <bladerboles@gmail.com>
2020-06-16 12:19:57 -0600

    Add useState hook.  Add default values for state hook.

--- src/CharacterGenerator.js
+++ src/CharacterGenerator.js
@@ -1,7 +1,56 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 export default function CharacterGenerator() {
-    return (
+  
+  const [state, setState] = useState({
+    new_character: {
+      character_owner: 0,
+      character_name: "New Character",
+      character_avatar: "https://www.placecage.com/100/100",
+      pet_name: "Killer",
+      pet_type: "Fish",
+      pet_avatar: "https://www.placecage.com/200/200"
+    },
+
+    new_portfolio: {
+      portfolio_owner: 0,
+      career_path: "Front End",
+      background: "Starving Artist",
+      personality: "Nerdy",
+      education: "Art",
+      bootcamp: "Good",
+      project_1_name: "Meme Generator 9002",
+      project_1_description: "Scouter breaking",
+      project_1_image: "https://www.placecage.com/155/155",
+      project_2_name: "Meme Generator 9003",
+      project_2_description: "Scouter breakore",
+      project_2_image: "https://www.placecage.com/255/255",
+      project_3_name: "Meme Generator 9004",
+      project_3_description: "It's over 8999+1!",
+      project_3_image: "https://www.placecage.com/355/355"
+    },
+
+    new_skills: {
+      skills_owner: 0,
+      front_end: 1,
+      back_end: 1,
+      computer_science: 1,
+      soft_skills: 1,
+      luck: 1
+    },
+
+    new_stats: {
+      stats_owner: 0,
+      funds: 100,
+      happiness: 100,
+      connections: 1,
+      energy: 100,
+      health: 100,
+      current_week: 1
+    }
+  });
+
+  return (
         <div>
             <h2>Create A Character</h2>
     <form>
