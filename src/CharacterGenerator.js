d1831f2d84b028a3a5285b0f6d3835f5799469d5
Blade <bladerboles@gmail.com>
2020-06-15 23:11:16 -0600

    Paste wireframe into CharacterGenerator.js.  Paste css in index.css.

--- src/CharacterGenerator.js
+++ src/CharacterGenerator.js
@@ -3,6 +3,121 @@ import React from 'react'
 export default function CharacterGenerator() {
     return (
         <div>
+            <h2>Create A Character</h2>
+    <form>
+      <label htmlFor="character-name">Character name: </label>
+      <input type="text" id="character-name" />
+      <label htmlFor="previous-job">
+        One day, something changed for you.  You just
+        couldn't do your old job as a(n)
+      </label>
+      <select name="previous-job" id="previous-job">
+        <option value="Accountant">Accountant</option>
+        <option value="Salesperson">Salesperson</option>
+        <option value="Artist">Starving Artist</option>
+        <option value="FryCook">Zadonald's Burger Flipper</option>
+        <option value="Criminal">Criminal Mastermind</option>
+      </select>
+      <label htmlFor="career-focus">
+        any longer.  After much soul and web searching,
+        you've decided to become a
+      </label>
+      <select name="career-focus" id="career-focus">
+        <option value="FrontEnd">Front End Developer.</option>
+        <option value="BackEnd">Back End Developer.</option>
+        <option value="FullStack">Full Stack Developer.</option>
+      </select>
+      <label htmlFor="left-job">
+        After settling on your new career path, you
+      </label>
+      <select name="left-job" id="left-job">
+        <option value="RageQuit">
+          snapped at your manager Al and rage quit.
+          (-1 Connection, Start at week 1)
+        </option>
+        <option value="TwoWeeks">
+          put in your two weeks notice and worked
+          it out. (+$1500, Start week 3)
+        </option>
+        <option value="FourWeeks">
+          were extra polite and worked out the month.
+          (+$3000, +1 Connection, Start week 5)
+        </option>
+      </select>
+      <label htmlFor="bootcamp">
+        After carefully weighing your options, you
+        decide to
+      </label>
+      <select name="bootcamp" id="bootcamp">
+        <option value="none">
+          complete the entire freeCodeCamp curriculum.
+          (Learn as you go)
+        </option>
+        <option value="scammy">
+          attend Scamzalot University.
+          (6 weeks)
+        </option>
+        <option value="good">
+          attend Okie Doke Academy.
+          (15 weeks)
+        </option>
+        <option value="great">
+          attend Mindfill Institute.
+          (24 weeks)
+        </option>
+      </select>
+      <label htmlFor="degree">
+        You hope your
+      </label>
+      <select name="degree" id="degree">
+        <option value="none">
+          lack of student debt
+        </option>
+        <option value="basketweaving">
+          BS in basketweaving
+        </option>
+        <option value="art">
+          BA in visual art
+        </option>
+        <option value="business">
+          MBA
+        </option>
+        <option value="biology">
+          Associates degree in Biology
+        </option>
+      </select>
+      <label htmlFor="personality">
+        and your
+      </label>
+      <select name="personality" id="personality">
+        <option value="artsy">
+          artsy fartsy personality
+        </option>
+        <option value="nerdy">
+          nerdy interests
+        </option>
+        <option value="charismatic">
+          people skills
+        </option>
+        <option value="basic">
+          love of pumpkin spice lattes
+        </option>
+      </select>
+      <label htmlFor="pet">
+        somehow come in handy!  Either way, you know
+        your pet
+      </label>
+      <select name="pet" id="pet">
+        <option value="dog">dog</option>
+        <option value="cat">cat</option>
+        <option value="fish">fish</option>
+      </select>
+      <label htmlFor="pet-name">
+        named
+      </label>
+      <input type="text" />
+      <p>will be there to support you!</p>
+    </form>
         </ div>
         
     )
